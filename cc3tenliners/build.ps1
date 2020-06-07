dd98bed6a7a2ddd0a40f2fde3f9918d857c4d9ef
Davy Mitchell <daftspaniel@gmail.com>
2020-06-07 20:46:32 +0100

    Restart. Partial game mechanic.

--- cc3tenliners/build.ps1
+++ cc3tenliners/build.ps1
@@ -6,6 +6,8 @@ if (Test-Path par.dsk){
 start-sleep 1
 decb dskini par.dsk -3
 decb copy par.bas -t par.dsk,P.BAS
+decb copy n.bas -t par.dsk,N.BAS
+decb copy t.bas -t par.dsk,T.BAS
 decb copy pal.bas -t par.dsk,PAL.BAS
-#echo start "" "C:\Program Files (x86)\VCC 2.0.1e\VCC.exe"
+Start-Process "C:\Program Files (x86)\VCC 2.0.1e\VCC.exe"
 exit
